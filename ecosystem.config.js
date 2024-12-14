6db95aa5e5bbc36d297fa751411a316bb5cffbdb
shubham_gopaisa <shubham.singh@gopaisa.in>
2024-12-14 12:58:02 +0530

    pm2 added

---
+++ ecosystem.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    apps: [
+      {
+        name: 'react-app',  // Name of your app
+        script: 'npm',       // Use npm to run the start script
+        args: 'start',       // The command to run your React app
+      }
+    ]
+  };
+  
